2c66010aeb58cbc333949f2e4ebdc47afb766fc5
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/multicoverage/MultiCoverageTest.scala
+++ src/test/scala/nl/biopet/tools/multicoverage/MultiCoverageTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.multicoverage
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object MultiCoverageTest extends BiopetTest {
+class MultiCoverageTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
