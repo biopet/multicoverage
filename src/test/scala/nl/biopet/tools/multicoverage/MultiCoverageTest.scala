b3d44b7091950f4467e7d72bccc2b83bec84f931
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:34:34 +0200

    Fixed compile issue

--- src/test/scala/nl/biopet/tools/multicoverage/MultiCoverageTest.scala
+++ src/test/scala/nl/biopet/tools/multicoverage/MultiCoverageTest.scala
@@ -7,7 +7,7 @@ object MultiCoverageTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
-      ToolTemplate.main(Array())
+      MultiCoverage.main(Array())
     }
   }
 }
