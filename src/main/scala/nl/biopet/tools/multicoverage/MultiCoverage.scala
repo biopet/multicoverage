7e2cf963df0454210a05a59e5eb3ac123f99f9b3
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-12-18 15:36:49 +0100

    changed args method

--- src/main/scala/nl/biopet/tools/multicoverage/MultiCoverage.scala
+++ src/main/scala/nl/biopet/tools/multicoverage/MultiCoverage.scala
@@ -14,7 +14,7 @@ import scala.concurrent.duration.Duration
 
 object MultiCoverage extends ToolCommand[Args] {
   def emptyArgs: Args = Args()
-  def argsParser = new ArgsParser(toolName)
+  def argsParser = new ArgsParser(this)
   def main(args: Array[String]): Unit = {
     val cmdArgs = cmdArrayToArgs(args)
 
