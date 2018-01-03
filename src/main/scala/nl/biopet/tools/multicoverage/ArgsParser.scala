c5937f0a4900b112805bf172f2003ea1c430e13d
DavyCats <davycats.dc@gmail.com>
2018-01-03 15:55:22 +0100

    remove unbounded

--- src/main/scala/nl/biopet/tools/multicoverage/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/multicoverage/ArgsParser.scala
@@ -27,7 +27,7 @@ import nl.biopet.utils.tool.{AbstractOptParser, ToolCommand}
 
 class ArgsParser(toolCommand: ToolCommand[Args])
     extends AbstractOptParser[Args](toolCommand) {
-  opt[File]('L', "bedFile") required () maxOccurs 1 unbounded () valueName "<file>" action {
+  opt[File]('L', "bedFile") required () maxOccurs 1 valueName "<file>" action {
     (x, c) =>
       c.copy(bedFile = x)
   } text "input bedfile"
@@ -35,11 +35,11 @@ class ArgsParser(toolCommand: ToolCommand[Args])
     (x, c) =>
       c.copy(bamFiles = x :: c.bamFiles)
   } text "input bam files"
-  opt[File]('o', "output") required () maxOccurs 1 unbounded () valueName "<file>" action {
+  opt[File]('o', "output") required () maxOccurs 1 valueName "<file>" action {
     (x, c) =>
       c.copy(outputFile = x)
   } text "output file"
-  opt[Unit]("mean") unbounded () valueName "<file>" action { (_, c) =>
+  opt[Unit]("mean") valueName "<file>" action { (_, c) =>
     c.copy(mean = true)
   } text "By default total bases is outputed, enable this option make the output relative to region length"
 }
