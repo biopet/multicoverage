6fac74a5d4d7ab792da3320583b401635690e036
pjvan_thof <pjrvanthof@gmail.com>
2018-02-15 15:50:13 +0100

    auto format

--- src/test/scala/nl/biopet/tools/multicoverage/MultiCoverageTest.scala
+++ src/test/scala/nl/biopet/tools/multicoverage/MultiCoverageTest.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2017 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.multicoverage
 
 import java.io.File
@@ -22,11 +43,11 @@ class MultiCoverageTest extends ToolTest[Args] {
     outputFile.deleteOnExit()
     MultiCoverage.main(
       Array("-L",
-        resourcePath("/rrna02.bed"),
-        "-b",
-        resourcePath("/paired01.bam"),
-        "-o",
-        outputFile.getAbsolutePath))
+            resourcePath("/rrna02.bed"),
+            "-b",
+            resourcePath("/paired01.bam"),
+            "-o",
+            outputFile.getAbsolutePath))
 
     Source.fromFile(outputFile).getLines().toList shouldBe List(
       "#contig\tstart\tend\tWipeReadsTestCase",
@@ -45,12 +66,12 @@ class MultiCoverageTest extends ToolTest[Args] {
     outputFile.deleteOnExit()
     MultiCoverage.main(
       Array("-L",
-        resourcePath("/rrna02.bed"),
-        "-b",
-        resourcePath("/paired01.bam"),
-        "-o",
-        outputFile.getAbsolutePath,
-        "--mean"))
+            resourcePath("/rrna02.bed"),
+            "-b",
+            resourcePath("/paired01.bam"),
+            "-o",
+            outputFile.getAbsolutePath,
+            "--mean"))
 
     Source.fromFile(outputFile).getLines().toList shouldBe List(
       "#contig\tstart\tend\tWipeReadsTestCase",
